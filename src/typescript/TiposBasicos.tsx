85b68d94018c1575d6d36e63ccd4f7e6aa391620
charlie <carlosalfonso.it@gmail.com>
2021-06-12 16:08:04 -0300

    first commit

---
+++ src/typescript/TiposBasicos.tsx
@@ -0,0 +1,21 @@
+
+export const TiposBasicos = () => {
+    
+    const nombre: string | number = 'Charlie';
+    const edad: number = 35;
+    const estaActivo: boolean = true;
+
+    const poderes: any[] = ['Velocidad', 'Volar', 'Respirar en el agua'];
+
+    poderes.push('algo');
+    poderes.push(2);
+
+    return (
+        <>
+            <h3>Tipos Básicos</h3>   
+            {nombre}, {edad}, {(estaActivo) ? 'Activo' : 'No Activo'}
+            <br/>
+            {poderes.join(' , ')}
+        </>
+    )
+}
